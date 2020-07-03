5e70ad3e0447ed2476543385e3124b911efa23cd
pandeyvibhuti <vibhutimp48@gmail.com>
2020-07-03 09:42:08 +0530

    new project commit

---
+++ src/main/java/com/jenkin/demojenkintest/DemojenkintestApplication.java
@@ -0,0 +1,13 @@
+package com.jenkin.demojenkintest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojenkintestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemojenkintestApplication.class, args);
+	}
+
+}
