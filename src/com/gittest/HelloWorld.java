947e731a6119224daafaff45e869e8f31e1bb5b0
pandeyvibhuti <34788649+pandeyvibhuti@users.noreply.github.com>
2020-07-03 09:12:53 +0530

    update the changes

--- src/com/gittest/HelloWorld.java
+++ src/com/gittest/HelloWorld.java
@@ -2,7 +2,7 @@ package com.gittest;
 
 public class HelloWorld {
 public static void main(String[] args) {
-	System.out.println("helloworld:::::::::::::Test");
+	System.out.println("helloworld TEST:::::::::::::");
 	System.out.println("helloworld12333");
 }
 }
